13596fd62cccea91417ea0e40e777b1690656467
Filipe Alvim <filipe2012alvim@gmail.com>
2023-07-13 15:14:14 -0300

    GET game by id

--- src/main/java/com/alvimm/dslist/services/GameService.java
+++ src/main/java/com/alvimm/dslist/services/GameService.java
@@ -1,10 +1,12 @@
 package com.alvimm.dslist.services;
 
+import com.alvimm.dslist.dto.GameDTO;
 import com.alvimm.dslist.dto.GameMinDTO;
 import com.alvimm.dslist.entities.Game;
 import com.alvimm.dslist.repositories.GameRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
@@ -12,6 +14,14 @@ import java.util.List;
 public class GameService {
     @Autowired
     private GameRepository gameRepository;
+
+    @Transactional(readOnly = true)
+    public GameDTO findById(Long id){
+        Game result = gameRepository.findById(id).get();
+        return new GameDTO(result);
+    }
+
+    @Transactional(readOnly = true)
     public List<GameMinDTO> findAll(){
         List<Game> result = gameRepository.findAll();
         return result.stream().map(x -> new GameMinDTO(x)).toList();
