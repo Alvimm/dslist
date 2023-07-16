1f9e0793de88e572f11e77d7f827110f157e0a26
Filipe Alvim <filipe2012alvim@gmail.com>
2023-07-16 13:48:00 -0300

    Homolog ok

--- src/main/java/com/alvimm/dslist/projections/GameMinProjection.java
+++ src/main/java/com/alvimm/dslist/projections/GameMinProjection.java
@@ -3,7 +3,7 @@ package com.alvimm.dslist.projections;
 public interface GameMinProjection {
     Long getId();
     String getTitle();
-    Integer getYear();
+    Integer getGameYear();
     String getImgUrl();
     String getShortDescription();
     Integer getPosition();
